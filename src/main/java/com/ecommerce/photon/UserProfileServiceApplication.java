0ac7d50dc69543d9bda97d9371ad1910e6577593
mohankumar4711 <36989835+mohankumar4711@users.noreply.github.com>
2018-03-14 12:04:58 +0530

    Update UserProfileServiceApplication.java
    import SpringBootServletInitializer;

--- src/main/java/com/ecommerce/photon/UserProfileServiceApplication.java
+++ src/main/java/com/ecommerce/photon/UserProfileServiceApplication.java
@@ -2,11 +2,12 @@ package com.ecommerce.photon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.ecommerce.photon")
-public class UserProfileServiceApplication {
+public class UserProfileServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserProfileServiceApplication.class, args);

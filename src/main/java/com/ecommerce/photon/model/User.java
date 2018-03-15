9d257b235ac1f9fec60ef21f7d38b170a61953cd
mohankumar4711 <36989835+mohankumar4711@users.noreply.github.com>
2018-03-15 13:12:52 +0530

    Update User.java

--- src/main/java/com/ecommerce/photon/model/User.java
+++ src/main/java/com/ecommerce/photon/model/User.java
@@ -41,6 +41,11 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", email=" + email + ", password=" + password + "]";
+	}
     
     
 }

d8fc9470e20cc7ec2f9b671b1ed3a27daa223b06
mohankumar4711 <36989835+mohankumar4711@users.noreply.github.com>
2018-03-15 13:12:25 +0530

    Update UserProfileController.java

--- src/main/java/com/ecommerce/photon/controller/UserProfileController.java
+++ src/main/java/com/ecommerce/photon/controller/UserProfileController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.photon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,15 @@ public class UserProfileController {
 	UserProfileService service;
 	
 	@RequestMapping(method=RequestMethod.GET,value="/getUser")
-	public User getUser(@RequestParam String email,@RequestParam String password) {
+	public User getUser(@RequestParam String email,@RequestParam String password) throws Exception {
+		System.out.println("From Controller======Email==========>"+email);
+		System.out.println("From Controller======password==========>"+password);
 		return service.getUserByEmail(email, password);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/registerNewUser")
+	public String registerNewUser(@RequestBody User user) {
+		System.out.println("From Controller================>"+user.toString());
+		return service.addNewUser(user);
 	}
 }

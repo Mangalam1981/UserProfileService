8fe958f196eb10aabc8fb9e1efc05bbb577e6962
mohankumar4711 <36989835+mohankumar4711@users.noreply.github.com>
2018-03-15 13:14:41 +0530

    Update UserProfileRepo.java

--- src/main/java/com/ecommerce/photon/repo/UserProfileRepo.java
+++ src/main/java/com/ecommerce/photon/repo/UserProfileRepo.java
@@ -4,5 +4,6 @@ import com.ecommerce.photon.model.User;
 
 public interface UserProfileRepo {
 
-	User findUserByEmail(String email, String password);
+	User findUserByEmail(String email, String password) throws Exception;
+	String addNewUser(User user);
 }

35fcaba14145ac887f7ee63e2c9dcf97f133e031
PHOTONINT\mohankumara_n <mohankumaramangalam.nagamanickam@photoninfotech.net>
2018-10-24 17:58:44 +0530

    Test case from userprofile

--- src/test/java/com/ecommerce/photon/UserProfileServiceApplicationTests.java
+++ src/test/java/com/ecommerce/photon/UserProfileServiceApplicationTests.java
@@ -1,16 +1,35 @@
 package com.ecommerce.photon;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ecommerce.photon.model.User;
+import com.ecommerce.photon.repo.UserProfileRepoImpl;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserProfileServiceApplicationTests {
-
+        
+    @Autowired
+    UserProfileRepoImpl userProfileRepoImpl; 
+	
 	@Test
-	public void contextLoads() {
+	public void userName() {	    
+	    try {
+	        String email = "mohan@test.com";
+	        String password = "mohan@123";
+	        User emailPassword = userProfileRepoImpl.findUserByEmail(email, password);
+	        System.out.println("emailPassword===================>" + emailPassword);
+            assertEquals(email, emailPassword.getEmail());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	    
 	}
 
 }
